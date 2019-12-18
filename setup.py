31058e65fb0c2f493f76f22b9bc990df6716d63e
steveoh <sgourley@utah.gov>
2019-12-18 16:17:12 -0700

    chore(release): 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swapper", # Replace with your own username
-    version="1.0.0",
+    version="1.0.1",
     author="Zach Beck",
     author_email="zbeck@utah.gov",
     description="Move data from one SDE database to another with minimal downtime",
